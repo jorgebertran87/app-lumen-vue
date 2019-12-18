02062799d483e9de228bb1138b711d23d451649f
Jorge Bertrán <jorgebertran87@gmail.com>
2019-12-18 14:11:34 +0100

    Add unit test for invalid first name

--- tests/unit/FirstNameTest.php
+++ tests/unit/FirstNameTest.php
@@ -4,24 +4,32 @@ declare(strict_types = 1);
 
 namespace UnitTests;
 
-use DateTimeImmutable;
 use PHPUnit\Framework\TestCase;
-use Src\Domain\Employee;
 use Src\Domain\FirstName;
 use Src\Domain\InvalidFirstNameException;
-use Src\Domain\InvalidGenderException;
-use Src\Domain\InvalidHireDateException;
-use Src\Domain\InvalidLastNameException;
 
 class FirstNameTest extends TestCase
 {
-    /** @test */
+    /**
+     * @test
+     * @throws InvalidFirstNameException
+     */
     public function itShouldCreateAValidFirstName() {
         $value = "FirstName";
 
         $firstName = new FirstName($value);
 
         $this->assertInstanceOf(FirstName::class, $firstName);
+    }
+
+    /**
+     * @test
+     * @throws InvalidFirstNameException
+     */
+    public function itShouldThrowAnExceptionForInvalidFirstName() {
+        $value = "";
+        $this->expectException(InvalidFirstNameException::class);
+        $firstName = new FirstName($value);
     }
 
 }
