22a95df5fc0bb567fd2f66bb9acb4e084e411519
Jorge Bertrán <jorgebertran87@gmail.com>
2019-12-18 10:58:35 +0100

    Add unit test for invalid first name

--- tests/unit/EmployeeTest.php
+++ tests/unit/EmployeeTest.php
@@ -27,13 +27,27 @@ class EmployeeTest extends TestCase
      * @test
      * @throws InvalidHireDateException
      */
-    public function itShouldThrowAnExceptionForIncorrectHireDate() {
+    public function itShouldThrowAnExceptionForInvalidHireDate() {
         $firstName = "FirstName";
         $lastName = "LastName";
         $gender = "M";
         $hireDate=new DateTimeImmutable("2020-03-01");
 
         $this->expectException(InvalidHireDateException::class);
+        $employee = new Employee($firstName, $lastName, $gender, $hireDate);
+    }
+
+    /**
+     * @test
+     * @throws InvalidFirstNameException
+     */
+    public function itShouldThrowAnExceptionForInvalidFirstName() {
+        $firstName = "";
+        $lastName = "LastName";
+        $gender = "M";
+        $hireDate=new DateTimeImmutable("2019-03-01");
+
+        $this->expectException(InvalidFirstNameException::class);
         $employee = new Employee($firstName, $lastName, $gender, $hireDate);
     }
 
