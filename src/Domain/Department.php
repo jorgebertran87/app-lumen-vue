71ade7aa8a5ec1298088108cfe1ba799e140e3cc
Jorge Bertrán <jorgebertran87@gmail.com>
2019-12-18 17:34:48 +0100

    Add Name VO to Department entity and fix unit test

--- src/Domain/Department.php
+++ src/Domain/Department.php
@@ -4,20 +4,16 @@ declare(strict_types=1);
 
 namespace Src\Domain;
 
-use Src\Domain\Employee\BirthDate;
-use Src\Domain\Employee\FirstName;
-use Src\Domain\Employee\Gender;
-use Src\Domain\Employee\HireDate;
-use Src\Domain\Employee\LastName;
+use Src\Domain\Department\Name;
 
 class Department
 {
     /**
-     * @var string
+     * @var Name
      */
     private $name;
 
-    public function __construct(string $name)
+    public function __construct(Name $name)
     {
         $this->name = $name;
     }
