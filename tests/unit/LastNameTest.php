0be8962a7c3fdef0725ea4c998c65392997cc7d7
Jorge Bertrán <jorgebertran87@gmail.com>
2019-12-18 14:21:05 +0100

    Pass unit test for invalid last name VO

--- tests/unit/LastNameTest.php
+++ tests/unit/LastNameTest.php
@@ -5,6 +5,7 @@ declare(strict_types = 1);
 namespace UnitTests;
 
 use PHPUnit\Framework\TestCase;
+use Src\Domain\InvalidLastNameException;
 use Src\Domain\LastName;
 
 class LastNameTest extends TestCase
@@ -16,5 +17,15 @@ class LastNameTest extends TestCase
         $firstName = new LastName($value);
 
         $this->assertInstanceOf(LastName::class, $firstName);
+    }
+
+    /**
+     * @test
+     * @throws InvalidLastNameException
+     */
+    public function itShouldThrowAnExceptionForInvalidLastName() {
+        $value = "";
+        $this->expectException(InvalidLastNameException::class);
+        $firstName = new LastName($value);
     }
 }
