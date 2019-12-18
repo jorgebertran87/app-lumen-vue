ad63adcf6ae93f88be6d6c9f4106080199a55fa0
Jorge Bertrán <jorgebertran87@gmail.com>
2019-12-18 10:13:23 +0100

    Add unit test for Employee

---
+++ tests/unit/EmployeeTest.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace UnitTests;
+
+use PHPUnit\Framework\TestCase;
+
+class EmployeeTest extends TestCase
+{
+    /** @test */
+    public function itShouldCreateAValidEmployee() {
+        $firstName = "FirstName";
+        $lastName = "LastName";
+        $gender = "M";
+        $hireDate="2019/03/01";
+
+        $employee = new Employee($firstName, $lastName, $gender, $hireDate);
+
+        $this->assertInstanceOf(Employee::class, $employee);
+    }
+
+}
