6fe6fde87a0792f48a9443848da707f52bf5cdc4
Jorge Bertrán <jorgebertran87@gmail.com>
2019-12-18 14:59:06 +0100

    Add unit test for hire date VO

---
+++ tests/unit/HireDateTest.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace UnitTests;
+
+use PHPUnit\Framework\TestCase;
+
+class HireDateTest extends TestCase
+{
+    /** @test */
+    public function itShouldCreateAValidHireDate() {
+        $value = "2019-05-20";
+
+        $hireDate = new HireDate($value);
+
+        $this->assertInstanceOf(HireDate::class, $hireDate);
+    }
+}
