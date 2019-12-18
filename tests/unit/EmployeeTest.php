f2301af292404f8dd0f03ff3a96209cd2f96cdf4
Jorge Bertrán <jorgebertran87@gmail.com>
2019-12-18 15:36:42 +0100

    Add birth date VO to Employee entity and pass unit test

--- tests/unit/EmployeeTest.php
+++ tests/unit/EmployeeTest.php
@@ -5,6 +5,7 @@ declare(strict_types = 1);
 namespace UnitTests;
 
 use PHPUnit\Framework\TestCase;
+use Src\Domain\BirthDate;
 use Src\Domain\Employee;
 use Src\Domain\FirstName;
 use Src\Domain\Gender;
@@ -15,12 +16,13 @@ class EmployeeTest extends TestCase
 {
     /** @test */
     public function itShouldCreateAValidEmployee() {
+        $birthDate = new BirthDate("1970-01-01");
         $firstName = new FirstName("FirstName");
         $lastName = new LastName("LastName");
         $gender = new Gender("M");
         $hireDate= new HireDate("2019-03-01");
 
-        $employee = new Employee($firstName, $lastName, $gender, $hireDate);
+        $employee = new Employee($birthDate, $firstName, $lastName, $gender, $hireDate);
 
         $this->assertInstanceOf(Employee::class, $employee);
     }
