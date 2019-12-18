7187cb25cd06ba745763fc64ebc0a036c39d7948
Jorge Bertrán <jorgebertran87@gmail.com>
2019-12-18 15:27:51 +0100

    Add unit test for birth date VO

---
+++ tests/unit/BirthDateTest.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace UnitTests;
+
+use PHPUnit\Framework\TestCase;
+use Src\Domain\FirstName;
+use Src\Domain\HireDate;
+use Src\Domain\InvalidFirstNameException;
+use Src\Domain\InvalidHireDateException;
+
+class BirthDateTest extends TestCase
+{
+    /** @test */
+    public function itShouldCreateAValidHireDate() {
+        $value = "2019-05-20";
+
+        $birthDate = new BirthDate($value);
+
+        $this->assertInstanceOf(BirthDate::class, $birthDate);
+    }
+}
