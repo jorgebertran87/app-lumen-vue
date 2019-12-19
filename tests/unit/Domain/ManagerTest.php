69d6b323cab8d69ffdd7953aa94c1033f296dddf
Jorge Bertrán <jorgebertran87@gmail.com>
2019-12-19 11:04:38 +0100

    Add unit test to add a department to a manager

--- tests/unit/Domain/ManagerTest.php
+++ tests/unit/Domain/ManagerTest.php
@@ -4,17 +4,35 @@ declare(strict_types = 1);
 
 namespace UnitTests\Domain;
 
+use DateTimeImmutable;
 use PHPUnit\Framework\TestCase;
+use Src\Domain\Department;
 use Src\Domain\Manager;
 
 class ManagerTest extends TestCase
 {
+    /** @var Manager */
+    private $manager;
+
+    protected function setUp(): void {
+        $employee = new FakeEmployee();
+        $this->manager = new Manager($employee);
+    }
+
     /** @test */
     public function itShouldCreateAValidManager() {
-        $employee = new FakeEmployee();
+        $this->assertInstanceOf(Manager::class, $this->manager);
+    }
 
-        $manager = new Manager($employee);
+    /** @test */
+    public function itShouldReturnADepartment() {
+        $department = new FakeDepartment();
+        $from = new DateTimeImmutable();
+        $to = new DateTimeImmutable();
+        $this->manager->addDepartment($department, $from, $to);
 
-        $this->assertInstanceOf(Manager::class, $manager);
+        $departments = $this->manager->departments();
+
+        $this->assertInstanceOf(Department::class, $departments[0]);
     }
 }
