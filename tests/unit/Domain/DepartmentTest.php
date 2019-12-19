a58dc618b178de23749c725b43e3bd7f648834df
Jorge Bertrán <jorgebertran87@gmail.com>
2019-12-19 11:04:38 +0100

    Add unit test to add a department to a manager

--- tests/unit/Domain/DepartmentTest.php
+++ tests/unit/Domain/DepartmentTest.php
@@ -6,15 +6,12 @@ namespace UnitTests\Domain;
 
 use PHPUnit\Framework\TestCase;
 use Src\Domain\Department;
-use Src\Domain\Department\Name;
 
 class DepartmentTest extends TestCase
 {
     /** @test */
     public function itShouldCreateAValidDepartment() {
-        $name = new Name("name");
-
-        $department = new Department($name);
+        $department = new FakeDepartment();
 
         $this->assertInstanceOf(Department::class, $department);
     }
