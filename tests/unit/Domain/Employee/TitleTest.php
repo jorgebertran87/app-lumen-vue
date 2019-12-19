b3bab40f3cbcefb64df1c07e534a389a230ca2f8
Jorge Bertrán <jorgebertran87@gmail.com>
2019-12-19 11:58:08 +0100

    Pass unit test for invalid Title VO

--- tests/unit/Domain/Employee/TitleTest.php
+++ tests/unit/Domain/Employee/TitleTest.php
@@ -6,17 +6,45 @@ namespace UnitTests\Domain\Employee;
 
 use DateTimeImmutable;
 use PHPUnit\Framework\TestCase;
+use Src\Domain\Employee\InvalidTitleException;
 use Src\Domain\Employee\Title;
 
 class TitleTest extends TestCase
 {
-    /** @test */
+    /**
+     * @test
+     * @throws InvalidTitleException
+     */
     public function itShouldCreateAValidTitle() {
-        $value = "2019-05-20";
+        $value = "title";
         $from = new DateTimeImmutable();
 
         $title = new Title($value, $from);
 
         $this->assertInstanceOf(Title::class, $title);
+    }
+
+    /**
+     * @test
+     * @throws InvalidTitleException
+     */
+    public function itShouldThrowAnExceptionForInvalidTitleValue() {
+        $value = "";
+        $from = new DateTimeImmutable();
+        $this->expectException(InvalidTitleException::class);
+        $title = new Title($value, $from);
+    }
+
+    /**
+     * @test
+     * @throws InvalidTitleException
+     */
+    public function itShouldThrowAnExceptionForInvalidTitleRange() {
+        $value = "title";
+        $from = new DateTimeImmutable('2019-05-01');
+        $to = new  DateTimeImmutable('2018-05-01');
+
+        $this->expectException(InvalidTitleException::class);
+        $title = new Title($value, $from, $to);
     }
 }
