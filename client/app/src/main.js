a54056346122c7e200fffcb454916d9023c9c562
Jorge Bertrán <jorgebertran87@gmail.com>
2019-12-22 00:36:01 +0100

    Create List component with Bootstrap table and pagination

--- client/app/src/main.js
+++ client/app/src/main.js
@@ -1,7 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import axios from 'axios'
+import BootstrapVue from 'bootstrap-vue'
+
+Vue.use(BootstrapVue)
 
 Vue.config.productionTip = false
+Vue.prototype.$http = axios
 
 new Vue({
   render: h => h(App),
