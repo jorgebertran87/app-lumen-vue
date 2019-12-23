a8c4f533a69410f8a348b8b0d17a5d5f594f29b8
Jorge Bertrán <jorgebertran87@gmail.com>
2019-12-23 17:21:07 +0100

    Add complete coverage for Department test

--- src/tests/unit/Domain/DepartmentTest.php
+++ src/tests/unit/Domain/DepartmentTest.php
@@ -9,10 +9,29 @@ use App\Domain\Department;
 
 class DepartmentTest extends TestCase
 {
+    /**
+     * @var FakeDepartment
+     */
+    private $department;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->department = new FakeDepartment('name');
+    }
+
     /** @test */
     public function itShouldCreateAValidDepartment() {
-        $department = new FakeDepartment('name');
+        $this->assertInstanceOf(Department::class, $this->department);
+    }
 
-        $this->assertInstanceOf(Department::class, $department);
+    /** @test */
+    public function itShouldReturnAValidName() {
+        $this->assertInstanceOf(Department\Name::class, $this->department->name());
+    }
+
+    /** @test */
+    public function itShouldReturnAValidId() {
+        $this->assertInstanceOf(Department\Id::class, $this->department->id());
     }
 }
