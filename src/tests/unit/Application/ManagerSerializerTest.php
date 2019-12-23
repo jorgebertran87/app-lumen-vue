e05b0b53dc251c1bed72e73127e380a95a60630a
Jorge Bertrán <jorgebertran87@gmail.com>
2019-12-23 18:51:44 +0100

    Add complete coverage for ManagerSerializer test

---
+++ src/tests/unit/Application/ManagerSerializerTest.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace UnitTests\Application;
+
+use App\Application\ManagerSerializer;
+use PHPUnit\Framework\TestCase;
+use UnitTests\Domain\FakeManager;
+
+class ManagerSerializerTest extends TestCase
+{
+    const SERIALIZER_KEYS = [
+        'id',
+        'firstName',
+        'lastName'
+    ];
+
+    /** @test */
+    public function itShouldReturnASerializedArray() {
+        $bus = new QueryBusStub();
+
+        $managerRepository = $bus->managerRepository();
+        $manager = FakeManager::withFirstDepartmentRange();
+        $managerRepository->add($manager);
+
+        $managerSerializer = new ManagerSerializer();
+        $serialized = $managerSerializer->serialize($manager);
+
+        foreach(self::SERIALIZER_KEYS as $key) {
+            $this->assertArrayHasKey($key, $serialized);
+        }
+    }
+}
