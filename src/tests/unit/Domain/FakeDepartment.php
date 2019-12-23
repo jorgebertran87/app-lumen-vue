728f18c9430f35ffcf305b3de5fca8191b79891a
Jorge Bertrán <jorgebertran@spotahome.com>
2019-12-23 14:14:39 +0100

    Add Id VO for department and fix broken tests

--- src/tests/unit/Domain/FakeDepartment.php
+++ src/tests/unit/Domain/FakeDepartment.php
@@ -6,11 +6,13 @@ namespace UnitTests\Domain;
 
 use App\Domain\Department;
 use App\Domain\Department\Name;
+use App\Domain\Department\Id;
 
 class FakeDepartment extends Department
 {
     public function __construct(string $name) {
         $name = new Name($name);
-        parent::__construct($name);
+        $id = new Id('d001');
+        parent::__construct($id, $name);
     }
 }
