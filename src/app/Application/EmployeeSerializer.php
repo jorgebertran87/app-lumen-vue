ab4e910541e7caf38c4a4f96dc94c18723aa1833
Jorge Bertrán <jorgebertran87@gmail.com>
2019-12-26 19:08:54 +0100

    Refactor EmployeeSerializer

--- src/app/Application/EmployeeSerializer.php
+++ src/app/Application/EmployeeSerializer.php
@@ -22,40 +22,62 @@ class EmployeeSerializer
         ];
 
         if (count($employee->departmentsRanges()) > 0) {
-            $result['departments'] = [];
-            /** @var DepartmentRange $departmentRange */
-            foreach($employee->departmentsRanges() as $departmentRange) {
-                $result['departments'][] = [
-                    "department" => $departmentRange->department()->name()->value(),
-                    "from" => $departmentRange->from()->value()->format('Y-m-d'),
-                    "to" => $departmentRange->to()->value()->format('Y-m-d')
-                ];
-            }
+            $result['departments'] = $this->addDepartments($employee);
         }
 
         if (count($employee->salaries()) > 0) {
-            $result['salaries'] = [];
-            /** @var Salary $salary */
-            foreach($employee->salaries() as $salary) {
-                $result['salaries'][] = [
-                    "salary" => number_format($salary->value()/100, 2, '.', ''),
-                    "from" => $salary->from()->format('Y-m-d'),
-                    "to" => $salary->to()->format('Y-m-d')
-                ];
-            }
+            $result['salaries'] = $this->addSalaries($employee);
         }
 
         if (count($employee->titles()) > 0) {
-            $result['titles'] = [];
-            /** @var Title $title */
-            foreach($employee->titles() as $title) {
-                $result['titles'][] = [
-                    "title" => $title->value(),
-                    "from" => $title->from()->format('Y-m-d'),
-                    "to" => $title->to()->format('Y-m-d')
-                ];
-            }
+            $result['titles'] = $this->addTitles($employee);
         }
+
+        return $result;
+    }
+
+    private function addDepartments(Employee $employee): array {
+        $result = [];
+
+        /** @var DepartmentRange $departmentRange */
+        foreach($employee->departmentsRanges() as $departmentRange) {
+            $result[] = [
+                "department" => $departmentRange->department()->name()->value(),
+                "from" => $departmentRange->from()->value()->format('Y-m-d'),
+                "to" => $departmentRange->to()->value()->format('Y-m-d')
+            ];
+        }
+
+        return $result;
+    }
+
+    private function addSalaries(Employee $employee): array {
+        $result = [];
+
+        /** @var Salary $salary */
+        foreach($employee->salaries() as $salary) {
+            $result[] = [
+                "salary" => number_format($salary->value()/100, 2, '.', ''),
+                "from" => $salary->from()->format('Y-m-d'),
+                "to" => $salary->to()->format('Y-m-d')
+            ];
+        }
+
+        return $result;
+    }
+
+    private function addTitles(Employee $employee): array {
+        $result = [];
+
+        /** @var Title $title */
+        foreach($employee->titles() as $title) {
+            $result[] = [
+                "title" => $title->value(),
+                "from" => $title->from()->format('Y-m-d'),
+                "to" => $title->to()->format('Y-m-d')
+            ];
+        }
+
         return $result;
     }
 }
